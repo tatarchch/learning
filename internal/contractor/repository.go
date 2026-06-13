66763b74c7c9d626a2db9718f088a76bb08588b4
tatar <tatarnirov15277@mail.ru>
2026-06-13 19:21:04 +0300

    add service method

--- internal/contractor/repository.go
+++ internal/contractor/repository.go
@@ -4,4 +4,6 @@ import "context"
 
 type repository interface {
 	Save(ctx context.Context, contractor Contractor) (Contractor, error)
+	FindById(ctx context.Context, id int64) (Contractor, error)
+	/*Update(ctx context.Context, contractor Contractor) error*/
 }
--- internal/contractor/service.go
+++ internal/contractor/service.go
@@ -2,8 +2,12 @@ package contractor
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"reflect"
 )
+
+var ErrInvalidId = errors.New("invalid ID. Id must be an integer and positive")
 
 type Service struct {
 	repo repository
@@ -29,4 +33,27 @@ func (s *Service) Create(ctx context.Context, input CreateInput) (Contractor, er
 	}
 
 	return created, nil
+}
+
+func (s *Service) Rename(ctx context.Context, id int64, name string) (Contractor, error) {
+	if id <= 0 || reflect.TypeOf(id).Name() != "int64" {
+		return Contractor{}, ErrInvalidId
+	}
+
+	contractor, err := s.repo.FindById(ctx, id)
+	if err != nil {
+		return Contractor{}, err
+	}
+
+	err = contractor.Rename(name)
+	if err != nil {
+		return Contractor{}, err
+	}
+
+	contractor, err = s.repo.Save(ctx, contractor)
+	if err != nil {
+		return Contractor{}, err
+	}
+
+	return contractor, nil
 }
