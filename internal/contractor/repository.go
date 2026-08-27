92ac543b66f9056b49832cd4bc2df294961bb8a7
tatarchch <tatarnirov15277@mail.ru>
2026-08-27 11:27:04 +0300

    ref

--- internal/contractor/repository.go
+++ internal/contractor/repository.go
@@ -1,23 +1,9 @@
 package contractor
 
-import (
-	"context"
-)
-
-type saverRepository interface {
-	Save(ctx context.Context, contractor Contractor) (Contractor, error)
-}
-
-type getterRepository interface {
-	FindByID(ctx context.Context, id int64) (Contractor, error)
-}
-
-type updaterRepository interface {
-	Update(ctx context.Context, contractor Contractor) (Contractor, error)
-}
+import "context"
 
 type repository interface {
-	saverRepository
-	getterRepository
-	updaterRepository
+	Save(ctx context.Context, contractor Contractor) (Contractor, error)
+	FindByID(ctx context.Context, id int64) (Contractor, error)
+	Update(ctx context.Context, contractor Contractor) (Contractor, error)
 }
